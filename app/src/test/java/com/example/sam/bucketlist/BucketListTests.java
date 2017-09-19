917e019d2554db48ce2e6f6aa5554ee416c877ff
live-beta <wanjala.sam.s@gmail.com>
2017-09-20 00:54:46 +0300

    ft(bucketlist test): add new bucketlist test
    - Ensures that the user can add new bucketlist
    
    [Builds #Task-0]

--- app/src/test/java/com/example/sam/bucketlist/BucketListTests.java
+++ app/src/test/java/com/example/sam/bucketlist/BucketListTests.java
@@ -1,8 +1,36 @@
 package com.example.sam.bucketlist;
 
+import android.util.Log;
+
+import com.example.sam.bucketlist.BucketList;
+import org.junit.Test;
+
+import java.text.DateFormat;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.zip.DataFormatException;
+
+import static org.junit.Assert.*;
 /**
  * Created by sam on 9/19/17.
  */
 
 public class BucketListTests {
+
+    Date date =new Date();
+
+    BucketListItems item = new BucketListItems(1, "Swimming", date ,false , 1);
+
+    List<ItemFields> newItem = item.createItem();
+
+    BucketList bucketList =new BucketList(1,"HAWAII",date, date, 1, newItem );
+
+    @Test
+    public void test_that_a_new_bucket_list_can_be_added_successfully(){
+
+        List<BucketListFields> value = bucketList.createBucketList() ;
+        assertEquals(value.get(0).getBucketListName(),"HAWAII");
+
+    }
 }
