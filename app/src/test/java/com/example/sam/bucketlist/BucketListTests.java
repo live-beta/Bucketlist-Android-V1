5582b6b71e3b77c404ece3ad3c05afcc9899b06b
live-beta <wanjala.sam.s@gmail.com>
2017-09-20 01:04:24 +0300

    update(bucketlist tests): add a setUp and teardown
    - Implements mock data to bucketlist tests
    
    [Builds #Task-0]

--- app/src/test/java/com/example/sam/bucketlist/BucketListTests.java
+++ app/src/test/java/com/example/sam/bucketlist/BucketListTests.java
@@ -3,6 +3,10 @@ package com.example.sam.bucketlist;
 import android.util.Log;
 
 import com.example.sam.bucketlist.BucketList;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.text.DateFormat;
@@ -18,13 +22,28 @@ import static org.junit.Assert.*;
 
 public class BucketListTests {
 
-    Date date =new Date();
+    public static BucketList bucketList;
+    public static List<ItemFields> newItem;
+    public static BucketListItems item;
 
-    BucketListItems item = new BucketListItems(1, "Swimming", date ,false , 1);
+    @BeforeClass
+    public static void setUp(){
+        Date date =new Date();
 
-    List<ItemFields> newItem = item.createItem();
+        item = new BucketListItems(1, "Swimming", date ,false , 1);
 
-    BucketList bucketList =new BucketList(1,"HAWAII",date, date, 1, newItem );
+        newItem = item.createItem();
+
+        bucketList =new BucketList(1,"HAWAII",date, date, 1, newItem );
+
+    }
+    @AfterClass
+    public static void tearDown(){
+        bucketList = null;
+        newItem =null;
+        item =null;
+
+    }
 
     @Test
     public void test_that_a_new_bucket_list_can_be_added_successfully(){
