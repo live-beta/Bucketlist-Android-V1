217aa8d8a5618564bdb499687d07d89f82f0b5e1
live-beta <wanjala.sam.s@gmail.com>
2017-09-19 22:49:14 +0300

    ft(bucketlists): add bucketlist fields
    - Add setters and getters for the bucketlist class
    
    [Builds #Task-0]

---
+++ app/src/main/java/com/example/sam/bucketlist/bucketListFields.java
@@ -0,0 +1,57 @@
+package com.example.sam.bucketlist;
+
+import java.util.Date;
+
+/**
+ * Created by sam on 9/19/17.
+ */
+
+public class bucketListFields {
+
+    private int id,userId;
+    private String bucketListName;
+    private Date dateCreated, dateModified;
+    private Items items;
+
+    public void setId(int idEntry){
+        this.id =idEntry;
+    }
+    public int getId(){
+        return id;
+    }
+    public void setUserId(int userIdEntry){
+        this.userId = userIdEntry;
+    }
+    public int getUserId(){
+        return userId;
+    }
+    public void setBucketListName(String bucketListNameEntry){
+        this.bucketListName = bucketListNameEntry;
+    }
+    public String getBucketListName(){
+        return bucketListName;
+    }
+    public void setDateCreated(Date dateCreatedEntry){
+        this.dateCreated = dateCreatedEntry;
+
+    }
+    public Date getDateCreated(){
+        return dateCreated;
+    }
+
+    public void setDateModified(Date dateModifiedEntry){
+        this.dateModified = dateModifiedEntry;
+    }
+    public Date getDateModified(){
+        return dateModified;
+    }
+
+    public void setItems(Items itemEntry){
+        this.items = itemEntry;
+    }
+    public Items getItems(){
+        return items;
+    }
+
+
+}
