1529641688e09706b5cac75d72b2a64c4362c79d
live-beta <wanjala.sam.s@gmail.com>
2017-11-07 17:09:10 +0300

    ft(delete bucketlist): delete method
    - Implements delete bucketlist API call
    
    [Builds #Task-api]

--- app/src/main/java/com/example/sam/bucketlist/Views/ItemsDetails.java
+++ app/src/main/java/com/example/sam/bucketlist/Views/ItemsDetails.java
@@ -1,5 +1,7 @@
 package com.example.sam.bucketlist.Views;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -22,9 +24,10 @@ import org.w3c.dom.Text;
 public class ItemsDetails extends AppCompatActivity {
 
     TextView bucketListID, bucketListName,newBucketListName;
-    Button editBucketlist;
+    Button editBucketlist, deleteBucketlist;
     ListView itemsDisplay;
     BucketListAPICalls apiCalls = new BucketListAPICalls();
+    private final Context context = this;
 
     @Override
    protected void onCreate(Bundle savedInstanceState){
@@ -34,6 +37,7 @@ public class ItemsDetails extends AppCompatActivity {
         bucketListID = (TextView)findViewById(R.id.bucketlistViewID);
         bucketListName =(TextView)findViewById(R.id.bucketlistViewName);
         newBucketListName =(TextView)findViewById(R.id.newBucketlistName);
+        deleteBucketlist =(Button)findViewById(R.id.bucketlist_delete);
         editBucketlist = (Button)findViewById(R.id.editBucketlistName);
         itemsDisplay =(ListView)findViewById(R.id.bucketlistItemList);
 
@@ -61,6 +65,34 @@ public class ItemsDetails extends AppCompatActivity {
                 }catch (Exception e){
                     Log.d("HTTP put Error",e.getMessage());
                 }
+
+
+            }
+        });
+
+        deleteBucketlist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                try {
+                    int bucketlistID = Integer.parseInt(extras.getString("Id"));
+                    boolean deleteStatus = apiCalls.deleteBucketList(bucketlistID);
+
+                    if (deleteStatus == true){
+
+                        Toast.makeText(getApplicationContext(),"Delete Success",Toast.LENGTH_SHORT).show();
+
+                        Intent intent = new Intent(context,BucketlistHome.class);
+                        startActivity(intent);
+
+                    }else {
+                        Toast.makeText(getApplicationContext(),"Delete Failure",Toast.LENGTH_SHORT).show();
+                    }
+                }catch (Exception e){
+                    Log.d("Error Deleting", e.getMessage());
+                }
+
 
 
             }
--- app/src/main/java/com/example/sam/bucketlist/Views/Login.java
+++ app/src/main/java/com/example/sam/bucketlist/Views/Login.java
@@ -72,6 +72,7 @@ public class Login extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+
             }
         });
     }
