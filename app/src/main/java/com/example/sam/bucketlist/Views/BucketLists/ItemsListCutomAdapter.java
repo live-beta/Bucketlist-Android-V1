1a591477686f15b2cf5369c23160525b613d1da9
live-beta <wanjala.sam.s@gmail.com>
2018-02-05 14:46:03 +0300

    Code Updates

---
+++ app/src/main/java/com/example/sam/bucketlist/Views/BucketLists/ItemsListCutomAdapter.java
@@ -0,0 +1,103 @@
+package com.example.sam.bucketlist.views.bucketLists;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.sam.bucketlist.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by sam on 11/15/17.
+ */
+
+
+/**
+ * TODO
+ *
+ * List the items Data
+ */
+public class ItemsListCutomAdapter extends RecyclerView.Adapter<ItemsListCutomAdapter.CustomViewAdapter>{
+
+
+    private LayoutInflater layoutInflater;
+    private ArrayList<HashMap> data;
+
+
+    public ItemsListCutomAdapter(Context context, ArrayList<HashMap> itemsData){
+
+
+        this.data = itemsData;
+        this.layoutInflater = LayoutInflater.from(context);
+
+    }
+
+    @Override
+    public CustomViewAdapter onCreateViewHolder(ViewGroup parent, int viewType){
+
+        View view =layoutInflater.inflate(R.layout.items_view_adapter,parent,false);
+
+        CustomViewAdapter viewHolder = new CustomViewAdapter(view);
+
+        return viewHolder;
+    }
+
+
+    @Override
+    public void onBindViewHolder(CustomViewAdapter holder, int position){
+
+        HashMap currentItemsObject  = data.get(position);
+        holder.setData(currentItemsObject,position);
+    }
+
+    @Override
+    public  int getItemCount(){
+        return data.size();
+    }
+
+
+
+class CustomViewAdapter extends RecyclerView.ViewHolder{
+
+    TextView id, itemName;
+    ImageView deleteBucketlist;
+
+    public CustomViewAdapter(View itemsView ){
+        super(itemsView);
+
+        // get the views
+
+        id = (TextView) itemsView.findViewById(R.id.idView);
+        itemName = (TextView) itemsView.findViewById(R.id.itemName);
+        deleteBucketlist = (ImageView)itemsView.findViewById(R.id.deleteBucketlist);
+
+    }
+    public void setData(final HashMap current,final int position){
+
+        final Context context  = layoutInflater.getContext();
+
+        id.setText(current.get("Id").toString());
+        itemName.setText(current.get("items").toString());
+        deleteBucketlist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(context," "+ current.get("items"),Toast.LENGTH_SHORT).show();
+//                BucketListAPICalls apiCalls = new BucketListAPICalls();
+//                apiCalls.deleteBucketList(Integer.parseInt(current.get("id").toString()));
+//                Toast.makeText(context,"Delete Successful",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+}
+
+}
---
+++ app/src/main/java/com/example/sam/bucketlist/Views/Users/Login.java
@@ -0,0 +1,80 @@
+//package com.example.sam.bucketlist.views.Users;
+//
+//import android.app.Activity;
+//import android.os.StrictMode;
+//import android.os.Bundle;
+//import android.content.Context;
+//import android.content.Intent;
+//import android.view.View;
+//import android.widget.EditText;
+//import android.widget.Button;
+//import android.widget.Toast;
+//
+//import com.example.sam.bucketlist.bucketListMethods.BucketList;
+//import com.example.sam.bucketlist.R;
+//import com.example.sam.bucketlist.views.BucketListItems.BucketlistHome;
+//
+///**
+// * Created by Sam on 19/03/2017.
+// */
+//
+//public class Login extends Activity {
+//
+//    EditText userName,password;
+//    Button login,forgot_pass;
+//    final Context context = this;
+//
+//    @Override
+//    protected  void  onCreate(Bundle savedInstanceState){
+//
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.login_activity_layout);
+//
+//        if (android.os.Build.VERSION.SDK_INT > 9) {
+//            StrictMode.ThreadPolicy policy =
+//                    new StrictMode.ThreadPolicy.Builder().permitAll().build();
+//            StrictMode.setThreadPolicy(policy);
+//        }
+//
+//        userName = (EditText)findViewById(R.id.uname);
+//        password =(EditText)findViewById(R.id.upass);
+//        login = (Button) findViewById(R.id.login);
+//
+//        login.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View view) {
+//                BucketList bucketList = new BucketList(userName.getText().toString(),
+//                        password.getText().toString());
+//
+//
+//                boolean status = bucketList.login();
+//
+//                if (status){
+//
+//                    Intent intent = new Intent(context, BucketlistHome.class);
+//                    startActivity(intent);
+//
+//                }else{
+//                    Toast.makeText(getApplicationContext(),"Wrong Username or Password",
+//                            Toast.LENGTH_SHORT).show();
+//                }
+//
+//            }
+//        });
+//
+//        forgot_pass =(Button) findViewById(R.id.register);
+//
+//        forgot_pass.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View v) {
+//
+//
+//            }
+//        });
+//    }
+//
+//
+//
+//}
